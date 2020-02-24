a11f94f2dd8223c469a17cff29ee1435e3d2fe26
vicky <vgonsalves@arcfix.com>
2020-02-24 22:37:52 +0530

    Added deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,27 +1,24 @@
-# install AWS SDK
+#! /bin/bash
+# Push only if it's not a pull request
+if [ -z "$TRAVIS_PULL_REQUEST" ] || [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
+# Push only if we're testing the master branch
+if [ "$TRAVIS_BRANCH" == "master" ]; then
+
+# This is needed to login on AWS and push the image on ECR
+# Change it accordingly to your docker repo
 pip install --user awscli
 export PATH=$PATH:$HOME/.local/bin
+eval $(aws ecr get-login --no-include-email --region $AWS_DEFAULT_REGION)
 
-# install necessary dependency for ecs-deploy
-add-apt-repository ppa:eugenesan/ppa
-apt-get update
-apt-get install jq -y
-
-# install ecs-deploy
-curl https://raw.githubusercontent.com/silinternational/ecs-deploy/master/ecs-deploy | \
-  sudo tee -a /usr/bin/ecs-deploy
-sudo chmod +x /usr/bin/ecs-deploy
-
-# login AWS ECR
-eval $(aws ecr get-login --region us-east-1)
-
-# or login DockerHub
-# docker login --username $DOCKER_HUB_USER --password $DOCKER_HUB_PSW
-
-# build the docker image and push to an image repository
-docker build -t haoliangyu/ecs-auto-deploy .
-docker tag haoliangyu/ecs-auto-deploy:latest $IMAGE_REPO_URL:latest
-docker push $IMAGE_REPO_URL:latest
-
-# update an AWS ECS service with the new image
-ecs-deploy -c $CLUSTER_NAME -n $SERVICE_NAME -i $IMAGE_REPO_URL:latest
+# Build and push
+docker build -t $IMAGE_NAME .
+echo "Pushing $IMAGE_NAME:latest"
+docker tag $IMAGE_NAME:latest "$IMAGE_REPO_URL:latest"
+docker push "$IMAGE_REPO_URL:latest"
+echo "Pushed $IMAGE_NAME:latest"
+else
+echo "Skipping deploy because branch is not 'master'"
+fi
+else
+echo "Skipping deploy because it's a pull request"
+fi
