faae32446bbd8a29376b9493f4b8a30e81b462ae
vicky <vgonsalves@arcfix.com>
2020-03-06 19:41:17 +0530

    Added extra user seed

--- src/seed-scripts/user.seed.js
+++ src/seed-scripts/user.seed.js
@@ -4,9 +4,25 @@ import User from '../models/user.model';
 import logger from '../config/logger';
 import config from '../config/config';
 
+const users = [
+  config.defaultAdmin,
+  {
+    name: 'John Doe',
+    email: 'johndoe@email.com',
+    password: 'Somepass@1',
+    role: 'user',
+  },
+  {
+    name: 'John Doe2',
+    email: 'johndoe2@email.com',
+    password: 'Somepass@1',
+    role: 'user',
+  },
+];
+
 const SeedUserFn = async () => {
   await User.find({}).deleteMany();
-  await User.create(config.defaultAdmin);
+  await User.create(users);
   logger.info('User Seed Done');
 };
 
