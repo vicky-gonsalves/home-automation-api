91f3ce357e9d9f40484eced1269b296b9e3f04b2
vicky <vgonsalves@arcfix.com>
2020-03-20 16:56:52 +0530

    Enhanced validation

--- src/validations/subDeviceParam.validation.js
+++ src/validations/subDeviceParam.validation.js
@@ -82,8 +82,8 @@ const updateSubDeviceParamValueValidation = {
       .required()
       .pattern(new RegExp('^[A-Za-z_\\d]{10,20}$')),
     paramName: Joi.string()
-      .required()
-      .pattern(new RegExp('^[A-Za-z_\\d]{1,50}$')),
+      .valid('status', 'mode')
+      .required(),
   }),
   body: Joi.object()
     .keys({
